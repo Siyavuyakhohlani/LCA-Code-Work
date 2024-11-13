2ddc568b5d0a254188917a2f98ca442c2641074d
Siyavuyakhohlani <Siyavuyakhohlani2@gmail.com>
2024-11-13 14:27:09 +0200

    files

---
+++ 02 M1 - Frontend Web Development/03 - week 03 JavaScript/functions.js
@@ -0,0 +1,33 @@
+// function greet(name){
+//     console.log(`Hi ${name}`);
+    
+// }
+// greet('Siya')
+
+//with return key word
+
+// function returned(a){
+//     return a
+// }
+// console.log('the numder is ' + returned('forty five'));
+
+// let run = function(a){
+//     console.log('You are running' + a);
+// }
+// run('alone')
+
+//self-ivoking functions
+
+// (function(){
+//     console.log('haha I am anonymous');
+    
+// })()
+
+
+
+// arrow function
+
+let walk = ()=>{
+    console.log('You are walking');
+}
+walk()
